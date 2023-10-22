eb8cb8e2deaf2ed19a156818241db8c2b1188772
Nikita Sinitsyn <n.o.sinitsyn@ya.ru>
2023-10-22 21:28:41 +0400

    подготовка для сдачи

--- db.js
+++ db.js
@@ -6,11 +6,4 @@ const { db: { host, port, name } } = config;
 module.exports.connectDatabase = () => mongoose.connect(`mongodb://${host}:${port}/${name}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
-.then(() => {
-  console.log('Успешное подключение DB');
-})
-.catch((error) => {
-  console.log('Ошибка при подключении к базе данных:', error.name);
 });
-
